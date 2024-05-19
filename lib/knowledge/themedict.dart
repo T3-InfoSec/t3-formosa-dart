b8daed7b3eb6bfd03cff68c9462d9e3dba4ff10d
ntodoric <nedotodoric@proton.me>
2024-05-19 22:19:32 +0200

    Added improved test and completed ThemeDict class.

--- lib/knowledge/themedict.dart
+++ lib/knowledge/themedict.dart
@@ -1,6 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:tuple/tuple.dart';
+import 'package:collection/collection.dart';
+import 'package:quiver/iterables.dart';
+
+
 //Global variables
 String FILL_SEQUENCE_KEY    = "FILLING_ORDER";
 String NATURAL_SEQUENCE_KEY = "NATURAL_ORDER";
@@ -24,7 +28,9 @@ class ThemeDict {
     //variable used to store all information from theme json
     Map<String, dynamic>  inner_dict = Map<String, dynamic>();
 
-    ThemeDict.EmptyDict(){}
+    ThemeDict.EmptyDict(String theme){
+      default_theme = theme;
+    }
 
     ThemeDict(String theme) {
     /*Creates instance of ThemeDict.
@@ -79,7 +85,10 @@ class ThemeDict {
     Returns:
       ThemeDict - Instance of ThemeDict class made from value which corresponds to given key.
     */ 
+      if (inner_dict[key] != null){
         return  ThemeDict._private(default_theme, inner_dict[key]);
+      }
+      return ThemeDict.EmptyDict(default_theme);
     }
 
     void setItem(String item, Map<String, dynamic> value){
@@ -188,6 +197,9 @@ class ThemeDict {
             }
           }
         );
+        if (temp.isEmpty){
+          temp.add(" ");
+        } 
         return temp;
     }
 
@@ -215,28 +227,18 @@ class ThemeDict {
         return temp;
     }
   
-    List<String> mapping(){
-    /*Returns list of total words from inner dictionary marked with [MAPPING_KW].
+    ThemeDict mapping(){
+    /*Returns ThemeDict from inner dictionary marked with [MAPPING_KW].
   
     None.
 
     Returns:
-      List<String> - List of total words which are marked with [MAPPING_KW].
-    */
-      List<String> temp = [];
-      inner_dict.forEach((key, value) {
-          if (key == MAPPING_KW)
-              temp.addAll(List<String>.from(inner_dict[key]));
-          else 
-            {
-              if (value.runtimeType.toString().contains("Map")){
-                  if (value[MAPPING_KW]!=null)
-                      temp.addAll(List<String>.from(value[MAPPING_KW]));
-              }
-            }
-          }
-        );
-        return temp;
+      ThemeDict - Instance of ThemeDict class with key equal to inner dictionary [MAPPING_KW].
+    */   
+      if (getItem(MAPPING_KW).inner_dict.isNotEmpty){  
+        return ThemeDict._private(default_theme, getItem(MAPPING_KW).inner_dict);
+      } 
+      return ThemeDict.EmptyDict(default_theme);
     }
 
     int bit_length(){
@@ -373,7 +375,7 @@ class ThemeDict {
     Returns:
       int - The index of the word given in the natural speech of the sentence.
     */
-    List<String> natural_words = natural_order();
+    List<String> natural_words = natural_order(); 
     return natural_words.indexOf(syntactic_word);
     }
 
@@ -466,7 +468,6 @@ class ThemeDict {
       List<Tuple2<String,String>>  - List of pairs of restriction from a given sentence.
     */
     List<Tuple2<int,int>> indexes = restriction_indexes();
-    print(indexes);
     List<Tuple2<String,String>> result = [];
     for(Tuple2<int,int> element in indexes){
         result.add(Tuple2(sentence[element.item1],sentence[element.item2]));
@@ -474,20 +475,308 @@ class ThemeDict {
     return result; 
     }
 
+    Tuple2<int, int> getRelationIndexes(Tuple2 relation) {
+    /*Returns indexes from a given tuple of relation syntactic and mnemonic words
+    If any word given only leads and not led, find the indexes in the total_words list.
+    
+    [relation] (Tuple2) : The relation given to find the index from the lists
+                                It can be whether a tuple of syntactic leads and the mnemonic leads
+                                or a tuple of tuples of syntactic leads and led and mnemonic leads and led
+                                e.g. ("VERB", word_verb) or (("VERB", "SUBJECT"), (word_verb, word_subject))
+
+    Returns:
+       Tuple2<int,int>  - Tuple with the indexes found in the restriction relation.
+    */
+    var syntacticRelation = relation.item1;
+    var mnemonicRelation  = relation.item2;
+
+    //If argument is type of Tuple2<String, String>
+    if (syntacticRelation is String && mnemonicRelation is String) {
+      var syntLeads = syntacticRelation;
+      var mnemoLeads = mnemonicRelation;
+      var wordsList = getItem(syntLeads).total_words(); 
+      var mnemoIndex = natural_index(syntacticRelation);
+      var wordIndex = wordsList.indexOf(mnemoLeads);
+      return Tuple2(mnemoIndex, wordIndex);
+    }
+    //If argument is Tuple2<Tuple2, Tuple2> 
+    else {
+      String syntLeads   = syntacticRelation.item1;
+      String syntLed     = syntacticRelation.item2;
+      String mnemoLeads  = mnemonicRelation.item1;
+      String mnemoLed    = mnemonicRelation.item2;
+      int mnemoIndex = natural_index(syntLed);
+      var restrictionDict = getItem(syntLeads).getItem(syntLed).mapping();
+      List<String> wordsList = List<String>.from(restrictionDict.inner_dict[mnemoLeads]);
+      var wordIndex = wordsList.indexOf(mnemoLed);
+      return Tuple2(mnemoIndex, wordIndex); 
+    }
+  }
+ 
+    List<int> getNaturalIndexes(dynamic sentence) {
+    /*Return the indexes of a sentence from the lists ordered as natural speech of this theme
+    The sentence can be given as a list or a string and must be complete
+    otherwise raises ArgumentError exception.
+    
+    [sentence] (Tuple or String) :  The words to be searched, must have complete sentences
+
+    Returns:
+       List<int>  - The list of indexes of the words in this theme lists and ordered as the mnemonic given.
+    */ 
+      if (sentence.runtimeType == String){
+        sentence = sentence.toString().split(" ");
+      }
+      if (sentence.length != words_per_phrase()) {
+        var errorMessage = 'The number of words in sentence must be $words_per_phrase(), but it is ${sentence.length}';
+        throw ArgumentError(errorMessage);
+      }
+
+      List<int> wordIndexes = List.filled(sentence.length, 0);
+      var restrictionSequence = restriction_sequence();
+      var primeSyntacticLeads = prime_syntactic_leads();
+      var wordRestrictionPairs = restriction_pairs(sentence);
+      var eachRelation; 
+      List<Tuple2> restrictionRelation = zip([restrictionSequence, wordRestrictionPairs])
+      .map((tuple) => Tuple2(
+            tuple[0],
+            tuple[1], 
+          ))
+      .toList(); 
 
 
-// list of functionalities to be done
-// get_relation_indexes, 
-// get_natural_indexes, 
-// get_filling_indexes, 
-// get_phrase_amount,
-// get_sentences,
-// get_phrase_indexes,
-// get_lead_mapping,
-// get_led_by_index,
-// get_lead_list,
-// assemble_sentence,
-// get_sentences_from_bits,
+      for (var eachSyntacticLeads in primeSyntacticLeads) { 
+        eachRelation = Tuple2(eachSyntacticLeads, sentence[natural_index(eachSyntacticLeads)]);  
+        var mnemoIndex = getRelationIndexes(eachRelation).item1;
+        var wordIndex = getRelationIndexes(eachRelation).item2; 
+        wordIndexes[mnemoIndex] = wordIndex;
+      }
+     
+      for (var eachRelation in restrictionRelation) { 
+        var mnemoIndex = getRelationIndexes(eachRelation).item1; 
+        var wordIndex = getRelationIndexes(eachRelation).item2; 
+        wordIndexes[mnemoIndex] = wordIndex;
+      }  
+      return wordIndexes;
+    }
 
+    List<int> get_filling_indexes(dynamic sentence){
+    /*Return the indexes of a sentence from the lists ordered as the filling order of this theme
+    The sentence can be given as a list or a string and must be a complete sentence
+    otherwise raises ValueError exception
+    
+    [sentence] (Tuple or String) :  The words to be searched, must have complete sentences
+
+    Returns:
+      List<int>  - The list of indexes of the words in this theme lists and ordered as the filling order.
+    */
+    if (sentence.runtimeType == String){
+        sentence = sentence.toString().split(" ");
+      }
+    if (sentence.length != words_per_phrase()) {
+      var errorMessage = 'The number of words in sentence must be $words_per_phrase(), but it is ${sentence.length}';
+      throw ArgumentError(errorMessage);
+    }
+
+    List<int> result = [];
+
+    var word_natural_indexes = getNaturalIndexes(sentence);
+    var n_map = natural_map();
+    for (int index in n_map){
+      result.add(word_natural_indexes[index]);
+    } 
+    return result;
+    }
+
+    int get_phrase_amount(dynamic mnemonic){
+    /*Returns how many phrases are in the given mnemonic.
+    
+    [sentence] (List<String> or String) :  The mnemonic to get the amount of phrases, it can be a string or a list of words.
+
+    Returns:
+      int - Returns the amount of phrases of the given mnemonic.
+    */
+    // split string into list of words
+    if (mnemonic.runtimeType == String){
+        mnemonic = mnemonic.toString().split("");
+    } 
+
+    //remove empty elements from spliting
+    for (int i = mnemonic.length - 1; i >= 0; i--) { 
+      if (mnemonic[i] == '') { 
+        mnemonic.removeAt(i);
+      }
+    }
+    
+    var phrase_amount; 
+    var mnemonic_size = mnemonic.length;
+    var phrase_size   = words_per_phrase(); 
+    try{
+    phrase_amount = (mnemonic_size / phrase_size);
+    } catch(exception){
+      print(exception.toString());
+    } 
+    return phrase_amount.round();
+    }
+
+    List<List<String>> get_sentences(dynamic mnemonic){
+    /*Splits to list the sentences from a given mnemonic
+    
+    [sentence] (List<String> or String) :  The mnemonic to get the amount of phrases, it can be a string or a list of words.
+
+    Returns:
+      List<List<String>> - Return a list of sentences with the lists of words from the mnemonic.
+    */ 
+
+    //normalize mnemonic
+    if (mnemonic.runtimeType == String){
+      mnemonic = mnemonic.toString().split(" ");
+    }
+    for (int i = mnemonic.length - 1; i >= 0; i--) { 
+      if (mnemonic[i] == '') { 
+        mnemonic.removeAt(i);
+      }
+    } 
+    //prepare variables for generating list of sentences
+    var phrase_size   = words_per_phrase();
+    var phrase_amount = get_phrase_amount(mnemonic); 
+    //generate sentence for specific mnemonic
+    List<List<String>> sentences = List.generate(phrase_amount, (int eachPhrase) {
+          int start = phrase_size * eachPhrase;
+          int end = phrase_size * (eachPhrase + 1);
+          return mnemonic.sublist(start, end);
+    }); 
+    return sentences;
+    }
+
+    List<int> get_phrase_indexes(dynamic mnemonic){
+    /*Get the indexes of a given mnemonic from each sentence in it
+    The mnemonic can be given as a list or a string and must have complete sentences
+    otherwise raises ArgumentError exception.
+    
+    [mnemonic] (List<String> or String) :  The words to be searched, must have complete sentences.
+
+    Returns:
+      List<int> - Returns list of indexes of the words in this theme lists and ordered as the filling order
+    */ 
+    //normalize mnemonic
+    if (mnemonic.runtimeType == String){
+      mnemonic = mnemonic.toString().split(" ");
+    }
+    for (int i = mnemonic.length - 1; i >= 0; i--) { 
+      if (mnemonic[i] == '') { 
+        mnemonic.removeAt(i);
+      }
+    } 
+
+    var sentences = get_sentences(mnemonic);
+    List<int> indexes = [];
+
+    for (dynamic each_phrase in sentences){
+      for (int each_fill_index in get_filling_indexes(each_phrase)){
+        indexes.add(each_fill_index);
+      }
+    } 
+    return indexes;
+    }
+
+    ThemeDict get_lead_mapping(String syntactic_key){
+    /*Returns ThemeDict class which is led by specific syntactic word.
+    
+    [syntactic_key] (String) :  Syntactic word to be led.
+
+    Returns:
+      ThemeDict - Returns instance of ThemeDict class with inner dictionary led by syntactic word.
+    */ 
+    return getItem(getItem(syntactic_key).led_by()).getItem(syntactic_key).mapping();
+    }
+
+    int get_led_by_index(String syntactic_key){
+    /*Get the natural index of the leading word.
+    
+    [syntactic_key] (String) :  The leading word to get the natural index from.
+
+    Returns:
+        int - Returns natural index of the leading word.
+    */    
+    return natural_index(getItem(syntactic_key).led_by());
+    }
+
+
+    List get_lead_list(String syntactic_key, List sentence){
+    /*Get the list of words led by the given syntactic word.
+    
+    [syntactic_key] (String)       : The leading word to get the natural index from.
+    [sentence]      (List<String>) : The sentence to solve the leading when the desired list depends on the leading words.
+
+    Returns:
+        List<String> - Return the list led by a syntactic word.
+    */ 
+    var lead_list;
+    var prime_syntactic_list = prime_syntactic_leads();  
+    if (prime_syntactic_list.contains(syntactic_key)){  
+        lead_list = getItem(syntactic_key).total_words();
+    }
+    else{  
+        var mapping = get_lead_mapping(syntactic_key);  
+        var leading_word = sentence[get_led_by_index(syntactic_key)];  
+        lead_list = mapping.inner_dict[leading_word];
+    }
+    return lead_list; 
+    }
+
+    List<String> assemble_sentence(String data_bits){
+    /*Build sentence using bits given following the dictionary filling order.
+    
+    [data_bits] (String) :  The information as bits from the entropy and checksum.
+                            Each step from it represents an index to the list of led words.
+ 
+    Returns:
+        List<String> - Return resulting words ordered of sentence in natural language.
+    */ 
+    int bit_index = 0;
+    List<String> current_sentence = List.empty(growable: true);
+
+    var filling_order_list = filling_order(); 
+    
+    //populate empty list
+    for (int i = 0; filling_order_list.length>i;i++){
+      current_sentence.add(" ");
+    }
+    for (var syntactic_key in filling_order_list){
+        int bit_length = getItem(syntactic_key).bit_length(); 
+        //Integer from substring of zeroes and ones representing index of current word within its list
+        int word_index = int.parse(data_bits.substring(bit_index, bit_index + bit_length), radix: 2);
+  
+        bit_index += bit_length;
+
+        var list_of_words = get_lead_list(syntactic_key, current_sentence);
+        var syntactic_order = natural_index(syntactic_key); 
+        
+        current_sentence[syntactic_order] = list_of_words[word_index];
+    }
+    return current_sentence;
+    }
+
+    List get_sentences_from_bits(String data_bits){
+    /*Get the mnemonic sentences in the natural speech order from given string of bits.
+    
+    [data_bits] (String) :  The bits of the entropy and checksum to get the sentences from
+ 
+    Returns:
+        List<String> - Return a list of words forming the sentences of the mnemonic.
+    */ 
+    String data;
+    int bits_per_phrase_ = bits_per_phrase();
+    num phrases_amount = data_bits.length / bits_per_phrase_;
+
+    List sentences = [];
+    for (int phrase_index=0; phrases_amount>phrase_index; phrase_index++){
+        int sentence_index = bits_per_phrase_ * phrase_index;
+        data = data_bits.substring(sentence_index, sentence_index + bits_per_phrase_);
+        sentences.addAll(assemble_sentence(data));
+    }
+    return sentences;
+    }
+ 
 }
  
--- tests/themedict_test.dart
+++ tests/themedict_test.dart
@@ -4,7 +4,6 @@ import 'package:GreatWall/knowledge/themedict.dart';
 import 'package:tuple/tuple.dart';
 void main()  async {
     TestWidgetsFlutterBinding.ensureInitialized();
-    
 
     final bip39            = Mnemonic("BIP39");
     final medieval_fantasy = Mnemonic("medieval_fantasy");
@@ -117,9 +116,76 @@ void main()  async {
       }); 
 
 
-    //work in progress
-    
+    group('getRelationIndexes', () {
+    test('Test getRelationIndexes with strings.', () {
+      expect(medieval_fantasy.words_dictionary.getRelationIndexes(Tuple2("VERB", "borrow")), Tuple2(1,4));
+    });
 
+    test('Test getRelationIndexes with tuples.', () { 
+      expect(medieval_fantasy.words_dictionary.getRelationIndexes(Tuple2(Tuple2('VERB', 'SUBJECT'), Tuple2('borrow', 'wizard'))), Tuple2(0,63));
+    }); 
+  });
+
+  test('Test getNaturalIndexes with set of sentences.', () {
+      expect(medieval_fantasy.words_dictionary.getNaturalIndexes(["fisherman", "ask_for", "bronze", "chains", "spy", "roof"]), [23, 1, 4, 10, 58, 24]);
+      expect(medieval_fantasy.words_dictionary.getNaturalIndexes(["rider", "reach", "rusty", "gun", "guard", "cave"]),[48, 21, 20, 26, 22, 6]);
+    }); 
+
+
+
+   test('Test get_filling_indexes with set of sentences.', () {
+      expect(medieval_fantasy.words_dictionary.get_filling_indexes(["fisherman", "ask_for", "bronze", "chains", "spy", "roof"]), [1, 23, 10, 4, 58, 24]);
+      expect(medieval_fantasy.words_dictionary.get_filling_indexes(["rider", "reach", "rusty", "gun", "guard", "cave"]), [21, 48, 26, 20, 22, 6]);
+    });  
+
+    test('Test get_phrase_amount with set of words.', () {
+      expect(medieval_fantasy.words_dictionary.get_phrase_amount(["viscount", "borrow", "nickel", "boots", "astronomer", "monastery", "fisherman", "ask_for", "bronze", "chains", "spy", "roof", "rider", "reach", "rusty", "gun", "guard", "cave", "ventriloquist", "hide", "stimulating", "milk", "inventor", "circus", "wizard", "borrow", "wild", "flask", "tutor", "oracle"]),
+      5);
+      expect(medieval_fantasy.words_dictionary.get_phrase_amount(["vi", "bo", "ni", "bo", "as", "mo", "fi", "as", "br", "ch", "sp", "ro", "ri", "re", "ru", "gu", "gu", "ca", "ve", "hi", "st", "mi", "in", "ci", "wi", "bo", "wi", "fl", "tu", "or"]),
+      5);
+    });
+
+    test('Test get_sentences with set of words.', () {
+      var result = [["vi", "bo", "ni", "bo", "as", "mo"], ["fi", "as", "br", "ch", "sp", "ro"], ["ri", "re", "ru", "gu", "gu", "ca"], ["ve", "hi", "st", "mi", "in", "ci"], ["wi", "bo", "wi", "fl", "tu", "or"]];
+      expect(medieval_fantasy.words_dictionary.get_sentences("vi  bo  ni  bo  as  mo  fi  as  br  ch  sp  ro  ri  re  ru  gu  gu  ca  ve  hi  st  mi  in  ci  wi  bo  wi  fl  tu  or"),
+              result);
+      expect(medieval_fantasy.words_dictionary.get_sentences(["vi", "bo", "ni", "bo", "as", "mo", "fi", "as", "br", "ch", "sp", "ro", "ri", "re", "ru", "gu", "gu", "ca", "ve", "hi", "st", "mi", "in", "ci", "wi", "bo", "wi", "fl", "tu", "or"]),
+              result);
+      });
  
-    
+    test('Test get_phrase_indexes with list of words.', () {
+      var result = [4, 62, 4, 19, 4, 15, 1, 23, 10, 4, 58, 24, 21, 48, 26, 20, 22, 6, 13, 61, 38, 26, 23, 3, 4, 63, 24, 30, 59, 18];
+      expect(medieval_fantasy.words_dictionary.get_phrase_indexes(["viscount", "borrow", "nickel", "boots", "astronomer", "monastery", "fisherman", "ask_for", "bronze", "chains", "spy", "roof", "rider", "reach", "rusty", "gun", "guard", "cave", "ventriloquist", "hide", "stimulating", "milk", "inventor", "circus", "wizard", "borrow", "wild", "flask", "tutor", "oracle"]),
+              result);
+      });
+    test('Test get_phrase_indexes with string.', () {
+      var result = [4, 62, 4, 19, 4, 15, 1, 23, 10, 4, 58, 24, 21, 48, 26, 20, 22, 6, 13, 61, 38, 26, 23, 3, 4, 63, 24, 30, 59, 18];
+      expect(medieval_fantasy.words_dictionary.get_phrase_indexes( "viscount borrow nickel boots astronomer monastery fisherman ask_for bronze chains spy roof rider reach rusty gun guard cave ventriloquist hide stimulating milk inventor circus wizard borrow wild flask tutor oracle"),
+              result);
+      });
+
+    test('Test get_led_by_index with various strings.', () {
+       expect(medieval_fantasy.words_dictionary.get_led_by_index("SUBJECT"), 1);
+       expect(medieval_fantasy.words_dictionary.get_led_by_index("WILDCARD"), 0);
+       expect(medieval_fantasy.words_dictionary.get_led_by_index("ADJECTIVE"),3);
+
+      });
+
+    test('Test get_lead_list with various strings.', () {
+       expect(medieval_fantasy.words_dictionary.get_lead_list("SUBJECT", ["","borrow","","","",""]), contains("actor"));
+       expect(bip39.words_dictionary.get_lead_list("WORDS", [""]), containsAll(["speed", "thing"]));
+    });
+
+    test('Test assemble_sentence with various data bits.', () {
+      expect(medieval_fantasy.words_dictionary.assemble_sentence("000010110001110101010100011011001"), ["gardener", "ask_for", "rusty", "stylus", "brewer", "square"]);
+      expect(medieval_fantasy.words_dictionary.assemble_sentence("110110001101110000001110100111011"), ["blacksmith", "trust", "bronze", "sword", "mercenary", "temple"]);
+    });
+
+    test('Test assemble_sentence with various data bits.', () {
+      expect(medieval_fantasy.words_dictionary.get_sentences_from_bits("010000000001001110111010100101010111000100000110100001001000010101"),["acolyte", "create", "indigenous", "milk", "ninja", "farm", "duke", "unveil", "bended", "handcuffs", "general", "portal"]);
+      expect(medieval_fantasy.words_dictionary.get_sentences_from_bits("000110011101110011100111001110110000111111001000011000100101001110"), ["dancer", "bet", "spicy", "strawberry", "senator", "pyramid", "valkyrie", "bet", "oak", "lyre", "duke", "library"]);
+      expect(bip39.words_dictionary.get_sentences_from_bits("000111100111011110010110110111000110001111011001011110010101100101"), ["bunker", "royal", "require", "sibling", "nurse", "protect"]);
+      expect(cute_pets.words_dictionary.get_sentences_from_bits("101001100010001101110100101100100"), ["aware", "service_dog", "rescue", "bulky", "trainer", "box"]);
+    });
+    //work in progress
 }
