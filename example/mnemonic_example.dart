705a07eb628dfb184afda0c0ba04cd826acbfd19
MuhammadMuradG <muhammad.murad.eg@gmail.com>
2024-08-14 12:42:57 +0300

    Enhance example template by remove unnecessary imports.

--- example/mnemonic_example.dart
+++ example/mnemonic_example.dart
@@ -1,22 +1,18 @@
 import 'package:mnemonic/mnemonic.dart';
-import 'package:collection/collection.dart';
 
 void main() {
-
   Mnemonic m = Mnemonic("finances");
 
-  
-  Function eq = const ListEquality().equals;
+  List<int> randomEntropy = [33, 254, 255, 33, 255, 56, 18, 51];
+  String resultingMnemonic = m.toMnemonic(randomEntropy);
+  List<int> entropyFromMnemonic = m.toEntropy(resultingMnemonic);
 
-  List<int> randomEntropy = [33,254,255,33,255,56,18,51];  
-  var resultingMnemonic =  m.toMnemonic(randomEntropy);
   print(resultingMnemonic);
-  var entropyFromMnemonic = m.toEntropy(resultingMnemonic);
   print(entropyFromMnemonic);
-  if (eq(randomEntropy,entropyFromMnemonic)){
-    print("OK");
+
+  if (randomEntropy.toString() == entropyFromMnemonic.toString()) {
+    print("Equal!");
+  } else {
+    print("Not Equal!");
   }
-  else {
-    print("NOK");
-  } 
 }
