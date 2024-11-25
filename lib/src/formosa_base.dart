048a9a07c6a1ddbec9c9e83bafd6adafae6065b1
flatFeather <github.astronaut143@silomails.com>
2024-11-25 23:01:52 +0100

    Update formosa with attributes for object oriented aproach

--- lib/src/formosa_base.dart
+++ lib/src/formosa_base.dart
@@ -1,252 +1,79 @@
-import 'dart:convert';
-import 'dart:math';
-
-import 'package:binary/binary.dart';
 import 'package:crypto/crypto.dart';
-import 'package:unorm_dart/unorm_dart.dart' as unorm;
 
 import 'formosa_theme.dart';
 
 /// A wrap implementation upon bip39 for supporting semantically connected
 /// words implementation upon multiple supported themes.
 class Formosa {
-  FormosaTheme _formosaTheme;
+  final FormosaTheme _formosaTheme;
+  late List<int> _entropy;
+  late String _seed;
 
-  /// Creates a [Formosa] instance with specified [formosaTheme].
-  Formosa({required FormosaTheme formosaTheme}) : _formosaTheme = formosaTheme;
+  /// Creates an instance of Formosa using the given FormosaTheme and initial entropy.
+  ///
+  /// The seed is automatically derived based on the initial entropy provided
+  /// and the words defined by the specified FormosaTheme.
+  Formosa({
+    required FormosaTheme formosaTheme,
+    required List<int> entropy,
+  })  : _formosaTheme = formosaTheme {
+    _entropy = entropy;
+    _seed = _generateSeed(entropy);
+  }
 
+  /// Returns [formosaTheme] associated with this instance.
+  ///
+  /// The theme defines the logic for converting entropy bits into
+  /// mnemonic phrases and ensures compatibility with the desired word set.
   FormosaTheme get formosaTheme => _formosaTheme;
 
-  /// Returns in which themes list of words defined with [code] can be found.
+  /// Returns the current [entropy] of this Formosa instance.
   ///
-  /// If theme is ambiguous or there is multiple themes found info message
-  /// is returned.
-  String detectTheme(List code) {
-    var possibleThemes = findThemes();
-    List foundThemes = [];
-    for (String word in code) {
-      for (FormosaTheme theme in possibleThemes) {
-        if (theme.data.wordList().contains(word)) {
-          foundThemes.add(theme);
-        }
-      }
-    }
-    if (foundThemes.length > 1) {
-      return 'Theme is ambiguous.';
-    }
-    if (foundThemes.isEmpty) {
-      return "Didn't found appropriate theme.";
-    }
-    return foundThemes[0].name;
-  }
+  /// The entropy serves as the binary source for generating the mnemonic
+  /// seed phrase.
+  List<int> get entropy => _entropy;
 
-  // TODO: Complete the implementation.
-  String expand(dynamic formosa) {
-    return '';
-  }
-
-  /// Returns formosa words from [password].
+  /// Updates the [entropy] of this Formosa instance and recalculates the seed.
   ///
-  /// Recover the formosa words from the [password]. The [password]
-  /// contains the first letters of each word from the formosa.
-  // TODO: Complete the implementation.
-  String expandPassword(String password) {
-    int n = (isBip39Theme()) ? 4 : 2;
-
-    if (password.length % n != 0) {
-      return password;
-    }
-
-    return '';
+  /// Whenever the entropy changes, the seed is automatically re-generated
+  /// to reflect the new entropy state.
+  set entropy(List<int> newEntropy) {
+    _entropy = newEntropy;
+    _seed = _generateSeed(newEntropy);
   }
 
-  // TODO: Complete the implementation.
-  String expandWord(dynamic formosa) {
-    return '';
-  }
-
-  /// Returns a formatted [formosa] in unique way.
-  String formatFormosa(dynamic formosa) {
-    if (formosa is String) {
-      formosa = formosa.split(' ');
-    }
-
-    int n = (isBip39Theme()) ? 4 : 2;
-
-    String password = '';
-    String word;
-    // Concatenate the first n letters of each word in a single string
-    // If the word in BIP39 has 3 letters finish with "-"
-    for (String word_ in formosa) {
-      word = word_;
-      if (n > word.length) {
-        word += '-';
-      }
-      for (int i = 0; n > i; i++) {
-        password += word[i];
-      }
-    }
-    return password;
-  }
-
-  /// Returns pattern which complication is measured by [strength] parameter.
-  String generate(int strength) {
-    if ((strength % 32 != 0) && (strength > 256)) {
-      return ('Strength should be below 256 and a multiple of 32, but it is$strength');
-    }
-    var rand = Random();
-    double strength_ = strength / 8;
-    return toFormosa(rand.nextInt(strength_.toInt()));
-  }
-
-  /// Check if the current used (selected) theme equal to BIP39.
+  /// Returns the mnemonic [seed] phrase derived from the current entropy.
   ///
-  /// Returns 'true' if current used theme is BIP39, else returns 'false'.
-  bool isBip39Theme() {
-    return formosaTheme == FormosaTheme.bip39;
-  }
+  /// The seed is expressed as a human-readable sequence of words, ensuring
+  /// compatibility with BIP39 or similar mnemonic standards.
+  String get seed => _seed;
 
-  /// The [toEntropy] method returns a entropy using provided one
-  /// or set of [words].
-  List<int> toEntropy(dynamic words) {
-    if (words is String) {
-      words = words.split(' ');
-    }
-
-    int wordsSize = words.length;
-    var wordsDict = formosaTheme.data;
-    int phraseAmount = wordsDict.getPhraseAmount(words);
-    int phraseSize = wordsDict.wordsPerPhrase();
-    int bitsPerChecksumBit = 33;
-    if ((wordsSize % phraseSize) != 0) {
-      // The number of [words] doesn't have good multiple.
-      return [0];
-    }
-    // Look up all the words in the list and construct the
-    // concatenation of the original entropy and the checksum.
-
-    // Determining strength of the password
-    int numberPhrases = wordsSize ~/ wordsDict.wordsPerPhrase();
-    int concatenationLenBits = numberPhrases * wordsDict.bitsPerPhrase();
-    int checksumLengthBits = concatenationLenBits ~/ bitsPerChecksumBit.round();
-    int entropyLengthBits = concatenationLenBits - checksumLengthBits;
-    var phraseIndexes = wordsDict.getPhraseIndexes(words);
-
-    List bitsFillSequence = [];
-    for (int i = 0; phraseAmount > i; i++) {
-      bitsFillSequence += formosaTheme.data.bitsFillSequence();
-    }
-
-    String concatenationBits = '';
-    for (int i = 0; phraseIndexes.length > i; i++) {
-      concatenationBits +=
-          (phraseIndexes[i].toRadixString(2).padLeft(bitsFillSequence[i], '0'));
-    }
-    List<int> entropy_ = List.filled(entropyLengthBits ~/ 8, 0);
-    int bitInt;
-
-    for (int entropyId = 0;
-        (entropyLengthBits / 8) > entropyId;
-        entropyId += 1) {
-      entropy_[entropyId] = 0;
-      for (int i = 0; 8 > i; i++) {
-        if (concatenationBits[(entropyId * 8) + i] == '1') {
-          bitInt = 1;
-        } else {
-          bitInt = 0;
-        }
-        entropy_[entropyId] |= (bitInt << (8 - 1 - i));
-      }
-    }
-    List<int> entropy = List.filled(entropyLengthBits ~/ 8, 0);
-    for (int entropyId = 0;
-        (entropyLengthBits / 8) > entropyId;
-        entropyId += 1) {
-      entropy[entropyId] = int.parse(entropy_[entropyId].toString());
-    }
-    return entropy;
-  }
-
-  /// Returns formosa in Formosa standard from given entropy [data]. If
-  /// input data is not appropriate or number of bytes is not adequate
-  /// info message is returned.
-  String toFormosa(dynamic data) {
+  /// Generates the mnemonic [seed] phrase based on the given [entropy].
+  ///
+  /// This process involves converting entropy bits into mnemonic phrases
+  /// using the logic defined in the associated FormosaTheme. The seed
+  /// reflects the checksum-integrated mnemonic representation of the entropy.
+  String _generateSeed(List<int> entropy) {
     int leastMultiple = 4;
-
-    if (data is! String && data is! List<int>) {
-      return 'Input data is not byte or string.';
+    if (entropy.isEmpty || entropy.length % leastMultiple != 0) {
+      throw ArgumentError(
+          'Entropy length must be a non-zero multiple of $leastMultiple');
     }
 
-    if ((data.length) % leastMultiple != 0) {
-      return 'Number of bytes should be multiple of $leastMultiple'
-          ' but it is ${data.length}';
-    }
+    var hashObject = sha256.convert(entropy);
 
-    if (data is String) {
-      data = utf8.encode(data);
-      int paddingLength =
-          ((leastMultiple - (data.length % leastMultiple))) % leastMultiple;
-      List<int> padding = [];
-      for (int i = 0; paddingLength > i; i++) {
-        padding.add(32);
-      }
-      data = data + padding;
-    }
+    String entropyBits = entropy
+        .map((e) => e.toRadixString(2).padLeft(8, '0'))
+        .join();
 
-    var hashObject = sha256.convert(data);
-
-    String entropyBits = '';
-    String binaryRepresentation;
-    for (int data_ in data) {
-      binaryRepresentation = data_.toRadixString(2);
-      if (binaryRepresentation.length < 8) {
-        while (binaryRepresentation.length != 8) {
-          binaryRepresentation = '0$binaryRepresentation';
-        }
-      }
-      entropyBits += binaryRepresentation;
-    }
-
-    String checksumBits_ = '';
-    String checksumBits = '';
-    for (int data_ in hashObject.bytes) {
-      binaryRepresentation = data_.toRadixString(2);
-      //add leading zeros
-      if (binaryRepresentation.length < 8) {
-        while (binaryRepresentation.length != 8) {
-          binaryRepresentation = '0$binaryRepresentation';
-        }
-      }
-      checksumBits_ += (binaryRepresentation);
-    }
-
-    double checksumLength_ = data.length * 8 / 32;
-    int checksumLength = checksumLength_.toInt();
-    for (int i = 0; checksumLength > i; i++) {
-      checksumBits += checksumBits_.toBitList()[i].toString();
-    }
+    String checksumBits = hashObject.bytes
+        .map((e) => e.toRadixString(2).padLeft(8, '0'))
+        .join()
+        .substring(0, entropy.length * 8 ~/ 32);
 
     String dataBits = entropyBits + checksumBits;
 
     List sentences = formosaTheme.data.getSentencesFromBits(dataBits);
-
-    String formosa = sentences.join(' ');
-    return formosa;
-  }
-
-  /// Returns a list of all supported themes.
-  static List<FormosaTheme> findThemes() {
-    return FormosaTheme.values;
-  }
-
-  /// Returns a normalized [bytesText] to normal from [unorm.NFKD] of Unicode.
-  static String normalizeBytes(List<int> bytesText) {
-    String unicodeText = utf8.decode(bytesText);
-    return unorm.nfkd(unicodeText);
-  }
-
-  /// Returns a normalized given [text] to normal from [unorm.NFKD] of Unicode.
-  static String normalizeString(String text) {
-    return unorm.nfkd(text);
+    return sentences.join(' ');
   }
 }
--- test/formosa_base_test.dart
+++ test/formosa_base_test.dart
@@ -1,49 +1,43 @@
 import 'package:t3_formosa/formosa.dart';
 import 'package:test/test.dart';
 
-void main() async {
-  final bip39 = Formosa(formosaTheme: FormosaTheme.bip39);
-  final bip39French = Formosa(formosaTheme: FormosaTheme.bip39French);
-  final cutePets = Formosa(formosaTheme: FormosaTheme.cutePets);
-  final medievalFantasy = Formosa(formosaTheme: FormosaTheme.medievalFantasy);
-  final sciFi = Formosa(formosaTheme: FormosaTheme.sciFi);
+void main() {
+  group('Formosa Class Tests', () {
+    late FormosaTheme theme = FormosaTheme.bip39;
+    late List<int> entropy = [0, 0, 0, 0];
 
-  test('Check does isBip39Theme function returns expected value.', () {
-    expect(bip39.isBip39Theme(), true);
-    expect(medievalFantasy.isBip39Theme(), false);
-    expect(cutePets.isBip39Theme(), false);
-    expect(sciFi.isBip39Theme(), false);
-  });
+    test('should initialize with provided entropy and generate seed', () {
+      final formosa = Formosa(formosaTheme: theme, entropy: entropy);
 
-  test('Check does detectTheme function finds appropriate theme.', () {
-    expect(bip39.detectTheme(['ability']), 'bip39');
-    expect(bip39.detectTheme(['aboutir']), 'bip39_french');
-    expect(medievalFantasy.detectTheme(['acidic']), 'medieval_fantasy');
-    expect(cutePets.detectTheme(['feeder']), 'cute_pets');
-    expect(sciFi.detectTheme(['4d']), 'sci_fi');
-  });
+      expect(formosa.entropy, equals(entropy));
 
-  test('Check does toFormosa function returns appropriate set of words.', () {
-    expect(bip39.toFormosa([0xAA, 0xAA, 0xAA, 0xAA]), 'primary fetch primary');
-    expect(bip39.toFormosa([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]),
-        'zoo zoo zoo zoo zoo zebra');
-    expect(sciFi.toFormosa([33, 254, 255, 33, 255, 56, 18, 51]),
-        'cyborg buy li-fi welder skeptic death_star sniper warn_about deuterium hover automaton habitat');
-    expect(bip39French.toFormosa([33, 254, 255, 33, 255, 56, 18, 51]),
-        'bourse véloce recevoir vorace inexact barbier');
-  });
+      expect(formosa.seed, isNotEmpty);
+    });
 
-  test('Check does formatFormosa function returns appropriate format.', () {
-    expect(bip39.formatFormosa(bip39.toFormosa([0xAA, 0xAA, 0xAA, 0xAA])),
-        'primfetcprim');
-    expect(
-        bip39.formatFormosa(
-            bip39.toFormosa([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])),
-        'zoo-zoo-zoo-zoo-zoo-zebr');
-  });
+    test('should throw ArgumentError when entropy length is not a multiple of 4', () {
+      final invalidEntropy = [0, 1, 2];
+      expect(
+        () => Formosa(formosaTheme: theme, entropy: invalidEntropy),
+        throwsA(isA<ArgumentError>()),
+      );
+    });
 
-  test('Check does to_entropy function returns good value.', () {
-    List<int> randomEntropy = [33, 255, 255, 33, 255, 56, 18, 51];
-    expect(bip39.toEntropy(bip39.toFormosa(randomEntropy)), randomEntropy);
+    test('should update seed when entropy is updated', () {
+      final formosa = Formosa(formosaTheme: theme, entropy: entropy);
+
+      final originalSeed = formosa.seed;
+
+      final newEntropy = [1, 1, 1, 1];
+      formosa.entropy = newEntropy;
+
+      expect(formosa.seed, isNot(originalSeed));
+    });
+
+    test('should generate valid seed based on entropy', () {
+      final formosa = Formosa(formosaTheme: theme, entropy: entropy);
+
+      expect(formosa.seed, isA<String>());
+      expect(formosa.seed.split(' ').length, greaterThan(1));
+    });
   });
 }
